434d40b59fd54bdcc85d8d57dfd08ee5e511f2bd
Cory Tak <cory.m.tak@hotmail.com>
2016-03-28 16:45:06 -0400

    Added the word friend

--- app/app.js
+++ app/app.js
@@ -12,7 +12,7 @@
         url: '/notes',
         template: '<h1>Notely</h1><p>{{ message }}</p>',
         controller: function($scope) {
-          $scope.message = "Welcome to Notely!";
+          $scope.message = "Welcome to Notely, friend!";
         }
       });
   }
