98680fcd649bf535d80641199c303a0405b34806
Cory Tak <cory.m.tak@hotmail.com>
2016-03-29 21:09:03 -0400

    Implement Notes Service Skeleton

---
+++ client/app/services/notes-service.js
@@ -0,0 +1,13 @@
+(function() {
+    angular.module('notely')
+        .service('NotesService', NotesService);
+    NotesService.$inject = ['$http'];
+    function NotesService($http) {
+        this.fetch = function() {
+            $http.get('http://localhost:3030')
+                .success(function() {
+                    console.log("It finished!");
+                });
+        };
+    }
+})();
--- server/app.js
+++ server/app.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var app = express();
+var notes = [
+    {
+        "title": "My cool note",
+        "body": "Cool isn't it?"
+    },
+    {
+        "title": "Whatevs",
+        "body": "Dawson's Creek"
+    }
+]
 
-app.get('/', function(request, response){
-    response.send('I <3 Express!!!!!');
+app.get('/', function(request, response) {
+    response.json(
+        [
+            {
+                title: "My cool note",
+                body: "Cool isn't it?"
+            },
+            {
+                title: "Whatevs",
+                body: "Dawson's Creek"
+            }
+        ]
+
+    );
 })
-.listen(3030, function(){
-    console.log("Listening on 3030 suckah");
-});
+    .listen(3030, function() {
+        console.log("Listening on 3030 suckah");
+    });
